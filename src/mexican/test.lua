4be7ed4e139d49438de35fdb0a913aebdc3b847b
nosyndicate <nosyndicate@gmail.com>
2016-04-28 18:40:02 -0400

    add mexican hat

---
+++ src/mexican/test.lua
@@ -0,0 +1,81 @@
+require 'torch'
+require 'nn'
+require 'rl'
+
+local hat = require 'mexican.hat'
+
+
+
+
+function main()
+	-- the model, input is just a single number as the state number
+	-- than we do a linearly transformation and then output three values and squash them into a distribution
+	local model1 = nn.Sequential():add(nn.Linear(1, 1))
+	local policy1 = rl.GaussianPolicy(1, 2)
+	local model2 = nn.Sequential():add(nn.Linear(1, 1))
+	local policy2 = rl.GaussianPolicy(1, 2)
+	local optimizer1 = rl.StochasticGradientDescent(model1:getParameters())
+	local optimizer2 = rl.StochasticGradientDescent(model2:getParameters())
+
+	local agent1 = rl.Reinforce(model1, policy1, optimizer1)
+	local agent2 = rl.Reinforce(model2, policy2, optimizer2)
+	agent1:setLearningRate(0.001)
+	agent2:setLearningRate(0.001)
+	
+	local state = torch.Tensor({1})
+	
+	print(model1:forward(state))
+	print(model2:forward(state))
+	
+	for i = 1,2000 do
+		local average1, average2 = 0,0
+		-- repeat 100 trials
+		for j = 1,100 do
+			agent1:startTrial()
+			agent2:startTrial()
+			local action1 = agent1:getAction(state)[1]
+			local action2 = agent2:getAction(state)[1]
+--			print("action1 is")
+--			print(action1)
+--			print("action2 is")
+--			print(action2)
+			local r, _ = hat:step(agent1:getAction(state)[1],agent2:getAction(state)[1])
+			--print(r)
+			agent1:step(state, r)
+			agent2:step(state, r)
+			agent1:endTrial()
+			agent2:endTrial()
+			average1 = average1 + r
+			average2 = average2 + r
+		end
+		agent1:learn(nil, nil)
+		agent2:learn(nil, nil)
+		average1 = average1/100
+		average2 = average2/100
+		if i%50==0 then
+			print("the norm of gradient is "..optimizer1.grads:norm().." and "..optimizer2.grads:norm())
+			print("average is")
+			print(average1)
+			print("model1")
+			print(model1:forward(state))
+			print("model2")
+			print(model2:forward(state))
+		end
+	end
+	
+	print(model1:forward(state))
+	print(model2:forward(state))
+	
+
+
+end
+
+
+main()
+
+
+
+
+
+
+
