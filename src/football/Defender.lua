eb303367003b8d7adbe887c70d37c16cc6441121
dwicke <drewwicke@gmail.com>
2016-05-09 18:06:43 -0400

    made function for defender to get the points defining the line segment that blocks end zone

--- src/football/Defender.lua
+++ src/football/Defender.lua
@@ -4,11 +4,13 @@ require 'torch'
 local Defender = torch.class('Defender')
 
 -- Constructor
-function Defender:__init(maxFieldSize, attackers, field, ypos)
+function Defender:__init(maxFieldSize, attackers, field, ypos, defenderLength)
   self.attackers = attackers
   self.field = field
   self.ypos = ypos
   self.xpos = maxFieldSize / 2.5
+  self.defenderLength = defenderLength
+  self.maxFieldSize
 
 end
 
@@ -24,7 +26,7 @@ function Defender:step()
   minDist = maxFieldSize + 1
   for i, l in ipairs(self.attackers) do
     if l:getY() - self.ypos < minDist then
-      vx = field:tdx(l:getX(), self.xpos)
+      vx = field:tdx(l:getX(), self.xpos) -- get the dist in the x direction (while wrapping around)
       if vx ~= 0.0 then
         sign = vx / math.abs(vx)
       else
@@ -35,11 +37,21 @@ function Defender:step()
   self.xpos = self.xpos + sign
 end
 
-function Defender:getStart()
+-- returns a set of four points so defined so that the first two points correspond
+-- to the beginning and end of the first segment and the next two points correspond
+-- to the second segment that wraps around to the other side of the field.
+-- so if not on the other side of the field.
+-- first return value is the number of points
+function Defender:getDefenderPoints()
 
+  endX = field:stx(self.xpos + self.defenderLength)
+
+  if endX < self.xpos then
+    -- then I have wrapped around and need to return 4 points
+    return 4, {{self.xpos, self.ypos}, {self.maxFieldSize, self.ypos},{0, self.ypos}, {endX, self.ypos}}
+  end
+   -- otherwise I am just a regular two point line segment
+  return 2, {{self.xpos, self.ypos}, {endX, self.ypos}}
 end
 
-function Defender:getEnd()
-
-end
 
